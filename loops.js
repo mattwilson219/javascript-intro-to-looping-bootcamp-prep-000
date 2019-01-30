e7675d444d8005809096c66383d58331f4d7110e
Matthew Wilson <wilsonmatt219@gmail.com>
2019-01-30 03:10:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,11 +21,11 @@ function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5;
   }
-}
+
 
 do {
   array.pop();
 } while (array.length > -1 && maybeTrue());
-
+    return array;
   
-
+}
